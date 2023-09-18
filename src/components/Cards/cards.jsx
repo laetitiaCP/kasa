a40420f3578f14111e214540c2a0d4aa2d34dcb1
laetitiaCP <laetitia.cendra.pro@gmail.com>
2023-09-18 20:13:50 +0200

    <fix><url cards>

--- src/components/Cards/cards.jsx
+++ src/components/Cards/cards.jsx
@@ -1,11 +1,12 @@
 import "./cards.scss";
+import {Link} from "react-router-dom";
 function Cards(props) {
     return (
         <div className="card">
-            <a className="card__wrap" href={props.link}>
+            <Link className="card__wrap" to={props.link} >
                 <img className="card__cover" src={props.cover} alt="cover"/>
                 <h2 className="card__title">{props.title}</h2>
-            </a>
+            </Link>
         </div>
     )
 }
